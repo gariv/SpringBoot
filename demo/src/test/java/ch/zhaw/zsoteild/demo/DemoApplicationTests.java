3babd64659052da4e484be59a6c866c945f0eef0
gariv <zsoterildikovirag@gmail.com>
2024-05-26 14:43:17 +0200

    initial commit

---
+++ demo/src/test/java/ch/zhaw/zsoteild/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.zhaw.zsoteild.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
